48fa3b6a9c23a2d774b8b5752c09ef264a76c831
kirannikam <nikamsarkar464@gmail.com>
2024-03-22 23:46:01 +0530

    234. Palindrome Linked List

---
+++ Daily Question/Leetcode_234.java
@@ -0,0 +1,47 @@
+/*
+    234. Palindrome Linked List
+    Given the head of a singly linked list, return true if it is a palindrome or false otherwise.
+
+    Example 1:
+    Input: head = [1,2,2,1]
+    Output: true
+
+    Example 2:
+    Input: head = [1,2]
+    Output: false
+    
+    Constraints:
+    The number of nodes in the list is in the range [1, 105].
+    0 <= Node.val <= 9 
+*/
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+Definition for singly-linked list.
+ */
+class Solution {
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+    public boolean isPalindrome(ListNode head) {
+        List<Integer> list = new ArrayList<>();;
+        while(head != null) {
+            list.add(head.val);
+            head = head.next;
+        }
+        
+        int left = 0;
+        int right = list.size()-1;
+        while(left < right && list.get(left) == list.get(right)) {
+            left++;
+            right--;
+        }
+        return left >= right;
+    }
+}
