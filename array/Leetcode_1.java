2c4683363a8ae2ee8ebd1f8b71fd37c52bcefd21
kirannikam <nikamsarkar464@gmail.com>
2024-04-06 23:35:23 +0530

    1. Two Sum

---
+++ array/Leetcode_1.java
@@ -0,0 +1,47 @@
+/*
+ * 1. Two Sum
+    Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+    You may assume that each input would have exactly one solution, and you may not use the same element twice.
+    You can return the answer in any order.
+
+    Example 1:
+    Input: nums = [2,7,11,15], target = 9
+    Output: [0,1]
+    Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+    
+    Example 2:
+    Input: nums = [3,2,4], target = 6
+    Output: [1,2]
+    
+    Example 3:
+    Input: nums = [3,3], target = 6
+    Output: [0,1]
+    
+    Constraints:
+    2 <= nums.length <= 10^4
+    -10^9 <= nums[i] <= 10^9
+    -10^9 <= target <= 10^9
+    Only one valid answer exists.
+*/
+
+public class Leetcode_1 {
+    public static int[] twoSum(int[] nums, int target) {
+        int[] ans = {-1,-1};
+        for (int i = 0; i < nums.length-1; i++) {
+            for (int j = i+1; j < nums.length; j++) {
+                if(nums[i] + nums[j] == target) {
+                    ans[0] = i;
+                    ans[1] = j;
+                    return ans;
+                }
+            }
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        int[] nums = {2,7,11,15};
+        int target = 9;
+        int[] ans = twoSum(nums, target);
+        System.out.println(ans[0] + " " + ans[1]);
+    }
+}
